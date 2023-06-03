bbd673c8f0bcba9fe622624ff9cd7f9032ddbea9
Forbesy <mvforbes21@gmail.com>
2023-06-02 23:09:43 -0400

    Completed Code

--- src/SalesTax.java
+++ src/SalesTax.java
@@ -2,10 +2,12 @@ public class SalesTax
 {
     public static void main(String[] args)
     {
-        double purchasePrice = 0;
+        double purchasePrice = 180;
         double SALES_TAX = .05;
+        double totalPrice = purchasePrice + (purchasePrice * SALES_TAX);
 
-        System.out.println("What is the price of your purchase?");
+        System.out.println("Your purchase price is " + purchasePrice + " and your sales tax is " + SALES_TAX + ". Your total is " + totalPrice);
+
 
 
     }
