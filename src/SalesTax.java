a992db2e5fcaa1d1c6f76dfcd88d790f0a503245
Forbesy <mvforbes21@gmail.com>
2023-06-02 22:56:15 -0400

    Initial commit

---
+++ src/SalesTax.java
@@ -0,0 +1,12 @@
+public class SalesTax
+{
+    public static void main(String[] args)
+    {
+        double purchasePrice = 0;
+        double SALES_TAX = .05;
+
+        System.out.println("What is the price of your purchase?");
+
+
+    }
+}
